8278a61ae58938029a569af0c3312f1739924627
Nathan Shostek <szos@posteo.net>
2021-01-19 12:30:49 +0100

    exported symbols

--- package.lisp
+++ package.lisp
@@ -1,4 +1,12 @@
 ;;;; package.lisp
 
 (defpackage #:cl-advice
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:symbol-not-fbound-error
+	   #:no-advisable-function-found-error
+	   #:with-advisable-object
+	   #:defadvice
+	   #:delete-advice
+	   #:activate-advice
+	   #:deactivate-advice
+	   #:advice-documentation))
