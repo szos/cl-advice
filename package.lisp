9a01663dfcd3af819af350904557e67f74194943
Nathan Shostek <szos@posteo.net>
2021-01-21 10:47:16 +0100

    exported symbols

--- package.lisp
+++ package.lisp
@@ -2,10 +2,23 @@
 
 (defpackage #:cl-advice
   (:use #:cl)
-  (:export #:symbol-not-fbound-error
+  (:export #:symbol-not-fbound-error ; conditions
 	   #:no-advisable-function-found-error
+	   ;; create advisable functions
+	   #:make-advisable
+	   #:defadvisable
+	   ;; advisable-function object access
 	   #:with-advisable-object
+	   #:with-advisable-object-advice
+	   #:with-inner-advice
+	   #:list-advice
+	   #:with-advice-list
+	   ;; advice creation
+	   #:add-advice
 	   #:defadvice
+	   ;; advice management
+	   #:pop-advice 
+	   #:advisable-function-p
 	   #:delete-advice
 	   #:activate-advice
 	   #:deactivate-advice
