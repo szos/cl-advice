4af1b1969f305685a3537405f5678cd384c70809
Mariano Montone <marianomontone@gmail.com>
2022-03-24 21:22:50 -0300

    Indentation

--- tests.lisp
+++ tests.lisp
@@ -33,19 +33,19 @@
 (def-test advice-tests ()
   (is (string= (foutput 'advisable-main) "main.") "Advisable function")
   (is (cl-advice:advisable-function-p (symbol-function 'advisable-main)) "Is advisable")
-  
+
   (is (string= (foutput 'main) "main.") "Without advice")
   (is (not (cl-advice:advisable-function-p (symbol-function 'main))) "Not advisable")
 
-  (signals error (cl-advice:add-advice :before 'main 'before) "Add advice to non advisable fails") 
-  
+  (signals error (cl-advice:add-advice :before 'main 'before) "Add advice to non advisable fails")
+
   (cl-advice:make-advisable 'main)
   (is (cl-advice:advisable-function-p (symbol-function 'main)) "Is advisable")
 
   (is (string= (foutput 'main) "main.") "Eval after advisable")
 
   (cl-advice:add-advice :before 'main 'before)
-  
+
   (is (string= (foutput 'main) "before.main.") "Before advice")
 
   (cl-advice:add-advice :before 'main 'before1)
@@ -53,13 +53,13 @@
 
   (cl-advice:replace-advice :before 'main 'before1 'before)
   (is (string= (foutput 'main) "before.before.main.") "Replace before advice")
-  
+
   (cl-advice:remove-advice :before 'main 'before)
   (cl-advice:remove-advice :before 'main 'before1)
 
   (is (string= (foutput 'main) "main.") "After remove :before advice")
 
-    (cl-advice:add-advice :after 'main 'after)
+  (cl-advice:add-advice :after 'main 'after)
   (is (string= (foutput 'main) "main.after.") "After advice")
 
   (cl-advice:remove-advice :after 'main 'after)
@@ -77,7 +77,7 @@
   (cl-advice:add-advice :around 'main 'around)
 
   (is (string= (foutput 'main) "before.begin.main.end.after.") "All advices")
-  
+
   (cl-advice:make-unadvisable 'main)
 
   (is (string= (foutput 'main) "main.") "After make-unadvisable")
